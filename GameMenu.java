036decb019cd548bb514593fc07df6865cdc180e
Falcho <m.k.falcham@gmail.com>
2024-02-29 13:59:51 +0100

    updates

--- GameMenu.java
+++ GameMenu.java
@@ -1,8 +1,9 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class GameMenu{
 	private ArrayList<String> actionsList = new ArrayList<>();
-
+	
 	public GameMenu(ArrayList<String> actionsList){
 		this.actionsList = actionsList;
 
@@ -13,7 +14,65 @@ public class GameMenu{
 			System.out.println(option);
 			// for loop oven over kan også skrives :
 			// actionsList.forEach(System.out::prinln);
+			}
+		}
+
+	public int getAction(ArrayList<String> actionsList){
+		this.actionsList = actionsList;
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Type a Number to choose an action");
+		displayMenu(actionsList);
+
+		int choice;
+
+		if(scan.hasNextInt()){
+		choice = scan.nextInt();
+			if(choice > 0 && choice < actionsList.size()){
+				return choice;
+			}else{
+			System.out.println("This is not a chooseable option");
+				choice = 0;
+			return getAction(actionsList);				
+			}
+		}else{
+			System.out.println("This is not a chooseable option");
+				choice = 0;
+			return getAction(actionsList);
+
 		}
 		
-		}
+
 	}
+
+
+
+ /*public int doAct() {
+
+	boolean isChoosing = true;
+
+
+	int choice;
+
+	while(isChoosing) {
+		displayMenu(actionsList);
+
+		choice = getUserInput("Make a choice xd");
+		if (choice > 0 && choice < actionsList.size()) {
+			isChoosing = false;
+			return choice;
+		} else {
+			System.out.println("Invalid choice - try again");
+		}
+
+	}
+	return 0;
+}
+
+public int getUserInput(String msg) {
+	System.out.println(msg);
+	Scanner scan  = new Scanner(System.in);
+	int input = scan.nextInt();
+return input;
+}*/
+}
--- TaskFour.java
+++ TaskFour.java
@@ -1,20 +1,42 @@
 import java.util.ArrayList;
 
+//Indeholder Task 4 og Task 5.
+
 public class TaskFour{
 
 	public static void main(String[]args){
 		ArrayList<String> actionsList = new ArrayList<>();
 
-		actionsList.add("Start game");
-		actionsList.add("Resume game");
-		actionsList.add("Pause game");
-		actionsList.add("End game");
+		actionsList.add("1) Start game");
+		actionsList.add("2) Resume game");
+		actionsList.add("3) Pause game");
+		actionsList.add("4) End game");
 
-		//System.out.println(actions.get(2)); // Forventet print : "Pause game"
+		//System.out.println(actionsList.get(2)); // Forventet print : "Pause game"
 
 		GameMenu menu = new GameMenu(actionsList);
 
-		menu.displayMenu(actionsList);
+		int userChoice = menu.getAction(actionsList);
+		System.out.println(userChoice);
+		doAction(userChoice);
 		
+	}
+
+	public static void doAction(int action){
+		switch(action){
+		case 1:
+			System.out.println("Starting the game now");
+			break;
+		case 2:
+			System.out.println("Fetching previously saved game data");
+			break;
+		case 3:
+			System.out.println("Game paused");
+			break;
+		case 4:
+			System.out.println("Ending game");
+			break;
+		}
+
 	}
 }
